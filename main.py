b3ebfe3039e6a6fff67396df99f469d05cd5a6c4
IvanovF <ivanovflorin2@yahoo.com>
2024-11-12 23:25:10 +0200

    added a few more columns

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from helping_functions import *
 
 weather_df = pd.read_csv("Tulcea.csv")
-weather_df.drop(["feelslikemax", "feelslikemin", "dew", "precipprob","precipcover", "preciptype", "snowdepth", "windgust", "winddir", "sealevelpressure",
+weather_df.drop(["feelslikemax", "feelslikemin", "dew", "precipprob","precipcover", "snowdepth", "windgust",
                         "cloudcover", "visibility", "solarradiation", "solarenergy", "severerisk", "moonphase", "conditions", "icon", "stations", "description", "temp"],
                 axis='columns', inplace=True)
 weather_df.rename(columns={"datetime" : "date"}, inplace=True)
@@ -25,6 +25,10 @@ print(weather_df.loc[1, "windspeed"])
 print(weather_df.loc[1, "uvindex"])
 print(weather_df.loc[1, "sunrise"])
 print(weather_df.loc[1, "sunset"])
+print(weather_df.loc[1, "winddir"])
+print(weather_df.loc[1, "preciptype"])
+print(weather_df.loc[1, "sealevelpressure"])
+
 
 
 
